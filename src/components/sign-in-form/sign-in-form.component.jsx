e67c3e6378763ca5675cbe1c27a5ea769266bc85
Saul Pereira <saul_pereira@hotmail.com>
2023-08-30 15:34:43 -0600

    110. Observer onAuthStateChange
    110. Observer onAuthStateChange

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -38,7 +38,6 @@ const SignInForm = () => {
 
         try {
             const {user} = await signInAuthUserWithEmailAndPassword(email, password);
-            setCurrentUser(user);
             
             resetFormFields();
 
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -6,7 +6,8 @@ import {
   GoogleAuthProvider,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
-  signOut
+  signOut,
+  onAuthStateChanged
 } from 'firebase/auth';
 import {
   getFirestore,
@@ -83,3 +84,7 @@ const firebaseConfig = {
   }
 
   export const signOutUser = async () => await signOut(auth);
+
+  export const onAuthStateChangedListener = (callback) => 
+    onAuthStateChanged(auth,callback);
+    
