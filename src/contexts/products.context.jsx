6aee2c2c847f9f63454912ed1e075965d59c73a7
Saul Pereira <saul_pereira@hotmail.com>
2023-09-01 16:27:56 -0600

    130. Get Products + Categories
    130. Get Products + Categories from Firestore

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,6 +1,6 @@
 import { createContext, useState, useEffect } from 'react';
 
-import { addCollectionAndDocuments } from '../utils/firebase/firebase.utils';
+import { getCategoriesAndDocuments } from '../utils/firebase/firebase.utils';
 
 import SHOP_DATA from '../shop-data.js';
 
@@ -10,6 +10,15 @@ export const ProductsContext = createContext({
 
 export const ProductsProvider = ({ children }) => {
     const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        const getCategoriesMap = async () => {
+            const categoryMap = await getCategoriesAndDocuments();
+            console.log(categoryMap);
+        }
+
+        getCategoriesMap();
+    }, []);
 
     // useEffect(() => {
     //     addCollectionAndDocuments('categories', SHOP_DATA);
