e418738db44fb30377dc295400d8035c7d73714d
Saul Pereira <saul_pereira@hotmail.com>
2023-09-01 16:58:50 -0600

    132. Using Our CategoriesMap
    132. Using Our CategoriesMap

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -4,17 +4,18 @@ import { getCategoriesAndDocuments } from '../utils/firebase/firebase.utils';
 
 import SHOP_DATA from '../shop-data.js';
 
-export const ProductsContext = createContext({
-    products: []
+export const CategoriesContext = createContext({
+    categoriesMap: {}
 });
 
-export const ProductsProvider = ({ children }) => {
-    const [products, setProducts] = useState([]);
+export const CategoriesProvider = ({ children }) => {
+    const [categoriesMap, setCategoriesMap] = useState({});
 
     useEffect(() => {
         const getCategoriesMap = async () => {
             const categoryMap = await getCategoriesAndDocuments();
             console.log(categoryMap);
+            setCategoriesMap(categoryMap);
         }
 
         getCategoriesMap();
@@ -24,6 +25,6 @@ export const ProductsProvider = ({ children }) => {
     //     addCollectionAndDocuments('categories', SHOP_DATA);
     // }, [])
 
-    const value = {products};
-    return ( <ProductsContext.Provider value={value}>{children}</ProductsContext.Provider> )
+    const value = {categoriesMap};
+    return ( <CategoriesContext.Provider value={value}>{children}</CategoriesContext.Provider> )
 }
