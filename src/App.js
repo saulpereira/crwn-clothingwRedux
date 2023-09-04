f4ad79dcd41191ff359749ec674302b2056a7d14
Saul Pereira <saul_pereira@hotmail.com>
2023-09-04 15:44:28 -0600

    138. Styled-Components Button
    138. Styled-Components Button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 
-import Navigation from './routes/navigation/navigation.jsx';
+import Navigation from './routes/navigation/navigation.component';
 import Home from './routes/home/home.component';
 import Authentication from './routes/authentication/authentication.component';
 import Shop from './routes/shop/shop.component';
