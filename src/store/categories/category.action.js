f209cea57c050f0c86a23709072999d3d15d7c9b
Saul Pereira <saul_pereira@hotmail.com>
2023-09-06 10:36:19 -0600

    158. Categories Selectors
    158. Categories Selectors

---
+++ src/store/categories/category.action.js
@@ -0,0 +1,4 @@
+import { CATEGORIES_ACTION_TYPES } from "./category.types";
+
+export const setCategoriesMap = (categoriesMap) => 
+    ({ type: CATEGORIES_ACTION_TYPES.SET_CATEGORIES_MAP, payload: categoriesMap});
